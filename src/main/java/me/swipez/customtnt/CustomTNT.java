ba7381ac4ab456daa2618a579bab745831d7541b
notnotnotswipez <strubbe06@gmail.com>
2021-05-28 22:41:21 -0400

    Actually remove command

--- src/main/java/me/swipez/customtnt/CustomTNT.java
+++ src/main/java/me/swipez/customtnt/CustomTNT.java
@@ -1,6 +1,5 @@
 package me.swipez.customtnt;
 
-import me.swipez.customtnt.command.ExplosionTest;
 import me.swipez.customtnt.listeners.TntPlaceListener;
 import me.swipez.customtnt.tnt.TntManager;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -12,7 +11,6 @@ public final class CustomTNT extends JavaPlugin {
     @Override
     public void onEnable() {
         plugin = this;
-        getCommand("explosiontest").setExecutor(new ExplosionTest());
         getServer().getPluginManager().registerEvents(new TntPlaceListener(), this);
         TntManager.initRecipes();
 
